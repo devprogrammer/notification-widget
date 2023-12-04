7300f33a5cc38cd2053bf2f3742469d8b77e8226
hiroshi <prodev0429@gmail.com>
2023-12-04 10:15:23 +0900

    update widget

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { getNotification } from './utils/api/getInfo';
 import logo from './assets/imgs/company_logo.png';
 import './App.css';
 
-function App(props) {
-  const  {symbol} = props
+function App() {
   const [notifications, setNotifications] = useState([]);
   const [show, setShow] = useState(true);
 
@@ -25,34 +24,39 @@ function App(props) {
   }
   useEffect(() => {
     fetchNotification();
+    // 
+    console.log('url', document.currentScript.getAttribute('src'))
+    var acc = new URL(document.currentScript.getAttribute('src')).searchParams.get("acc");
+    console.log('acc =====>', acc);
   }, [])
 
   useEffect(() => {
     setTimeout(() => {
       if (show)
         setShow(!show)
+        console.log("====", notifications)
     }, 10000);
   }, [notifications])
 
   // if (!show) return <></>
   return (
-    <>
+    <div className='absolute w-90 bottom-8 left-4'>
       {
         notifications && notifications.length > 0 && notifications?.map((n, i) => {
-          if (!n.status && show) 
+          if (!n.status) 
             return (
-              <div className="flex justify-between border border-1 shadow rounded-full gap-[12px] p-[4px] mt-[16px]" key={i}>
+              <div className="flex justify-between border shadow rounded-full gap-3 p-2 mt-4 z-[1000]" key={i}>
                 <img src={logo} className="rounded-full" alt="logo" width={78} height={78}/>
-                <div className='flex flex-1 flex-col truncate'>
-                  <h2 className='truncate font-[600]'>{n.type.toUpperCase()}</h2>
-                  <p className='truncate size-[14px]'>{n?.msg || `VISITORS: ${n?.message_visitors}`}</p>
-                  <p className='truncate font-[600] text-[12px]'>BY www.mymanager.com</p>
+                <div className='flex flex-1 flex-col gap-2 truncate'>
+                  <h2 className='truncate text-20 font-600'>{n.type.toUpperCase()}</h2>
+                  <p className='truncate text-14'>{n?.msg || `VISITORS: ${n?.message_visitors}`}</p>
+                  <p className='truncate font-600 text-12'>BY www.mymanager.com</p>
                 </div>
               </div>
             )
         })
       }
-    </>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,35 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('gamestop'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
 const root = ReactDOM.createRoot(document.getElementById('gamestop'));
 const widgetDiv = document.querySelector('.mymanager-notification-widget');
 
 root.render(
   <React.StrictMode>
-    <App symbol={widgetDiv?.dataset.symbol}/>
+    {/* <App symbol={widgetDiv?.dataset.symbol}/> */}
+    <App />
   </React.StrictMode>
 );
-
-// // Find all widget divs
-// const widgetDivs = document.querySelector('.mymanager-notification-widget');
-
-// // Inject our React App into each class
-// widgetDivs.forEach(div => {
-//     ReactDOM.render(
-//       <React.StrictMode>
-//         <App symbol={div.dataset.symbol}/>
-//       </React.StrictMode>,
-//         div
-//     );
-// });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
