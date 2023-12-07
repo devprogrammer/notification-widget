78ea38c9200716abbb71378d9872c2a246b3b5fc
Full Stack Web & Mobile Developer <devprogrammer22@gmail.com>
2023-12-07 19:31:06 +0900

    update

--- src/App.js
+++ src/App.js
@@ -84,39 +84,41 @@ function App() {
   const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
   if (!show) return <></>
-
-  return (
-    <div className='mymanager-widget-container'>
-      {
-        notifications && notifications.length > 0 && notifications?.map((n, i) => {
-          if (n.status) 
-            return (
-              <div className="mymanager-widget-body" key={i}>
-                <div className="mymanager-mapbox">
-                  <GoogleMapReact
-                    bootstrapURLKeys={{ key: "AIzaSyBVrhAX-Kht3yDvmUCQHqMSeB1Qd7XVFq8" }}
-                    defaultCenter={pos}
-                    defaultZoom={11}
-                    // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-                  >
-                    <AnyReactComponent
-                      lat={pos.lat}
-                      lng={pos.lng}
-                      text="My Marker"
-                    />
-                  </GoogleMapReact>
+  else {
+    return (
+      <div className='mymanager-widget-container'>
+        {
+          notifications && notifications.length > 0 && notifications?.map((n, i) => {
+            if (n.status) 
+              return (
+                <div className="mymanager-widget-body" key={i}>
+                  <div className="mymanager-mapbox">
+                    <GoogleMapReact
+                      bootstrapURLKeys={{ key: "AIzaSyBVrhAX-Kht3yDvmUCQHqMSeB1Qd7XVFq8" }}
+                      defaultCenter={pos}
+                      defaultZoom={11}
+                      // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+                    >
+                      <AnyReactComponent
+                        lat={pos.lat}
+                        lng={pos.lng}
+                        text="My Marker"
+                      />
+                    </GoogleMapReact>
+                  </div>
+                  <div className='mymanager-text-content mymanager-truncate'>
+                    <h2 className='mymanager-title mymanager-truncate'>{n.type.toUpperCase()}</h2>
+                    <p className='mymanager-event-content mymanager-truncate'>{n?.msg || `VISITORS: ${n?.message_visitors}`}</p>
+                    <p className='mymanager-company mymanager-truncate'>BY www.mymanager.com</p>
+                  </div>
                 </div>
-                <div className='mymanager-text-content mymanager-truncate'>
-                  <h2 className='mymanager-title mymanager-truncate'>{n.type.toUpperCase()}</h2>
-                  <p className='mymanager-event-content mymanager-truncate'>{n?.msg || `VISITORS: ${n?.message_visitors}`}</p>
-                  <p className='mymanager-company mymanager-truncate'>BY www.mymanager.com</p>
-                </div>
-              </div>
-            )
-        })
-      }
-    </div>
-  );
+              )
+          })
+        }
+      </div>
+    );
+  }
+  return <></>
 }
 
 export default App;
