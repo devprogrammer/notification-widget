41545944aa964c55fc776841de2d4c6a5ff328ea
Full Stack Web & Mobile Developer <devprogrammer22@gmail.com>
2023-12-08 12:43:57 +0900

    update

---
+++ src/components/notificatio.css.js
@@ -0,0 +1,55 @@
+export const mymanagerWidgetContainer = {
+  position: "absolute",
+  width: "360px",
+  bottom: "32px",
+  left: "15px",
+  zIndex: "1000"
+}
+export const mymanagerWidgetBody = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  border: "1px solid rgb(0 0 0 / 0export const 2)",
+  backgroundColor: "white",
+  boxShadow: "0 1px 3px 0 rgb(0 0 0 / 0export const 1), 0 1px 2px -1px rgb(0 0 0 / 0export const 1)",
+  borderRadius: "9999px",
+  gap: "24px",
+  padding: "10px 16px",
+  marginTop: "16px"
+}
+
+export const mymanagerMapbox = {
+  height: 70,
+  width: 70,
+  borderRadius: 9999,
+}
+
+export const mymanagerTruncate = {
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+  margin: "0 !important",
+}
+
+export const mymanagerTextContent = {
+  display: "flex",
+  flex: "1 1 0%",
+  flexDirection: "column",
+  gap: 8,
+}
+
+export const mymanagerTitle = {
+  fontSize: "16px",
+  fontWeight: 700,
+}
+
+export const mymanagerEventContent = {
+  fontSize: "14px",
+  fontWeight: 600,
+}
+
+export const mymanagerCompany = {
+  fontSize: "12px",
+  fontWeight: 600,
+}
+
--- src/components/notification.js
+++ src/components/notification.js
@@ -1,6 +1,7 @@
 import GoogleMapReact from 'google-map-react';
 import { useEffect, useState } from 'react';
 import { getLatLng } from '../utils/functions';
+import { mymanagerTextContent, mymanagerWidgetBody, mymanagerWidgetContainer, mymanagerTruncate, mymanagerTitle } from './notificatio.css';
 
 
 export const NotificationWrapper = ({settings, notification}) => {
@@ -42,15 +43,39 @@ export const NotificationWrapper = ({settings, notification}) => {
   
   const AnyReactComponent = ({ text }) => <div>{text}</div>;
   if (!info) return <></>
+  {/*<div className='mymanager-widget-container' style={{marginTop: '20px'}}>
+     <div className="mymanager-widget-body">
+      <div className="mymanager-mapbox">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyBVrhAX-Kht3yDvmUCQHqMSeB1Qd7XVFq8" }}
+          defaultCenter={pos}
+          defaultZoom={11}
+          // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        >
+          <AnyReactComponent
+            lat={pos.lat}
+            lng={pos.lng}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+      <div className='mymanager-text-content mymanager-truncate'>
+        <h2 className='mymanager-title mymanager-truncate'>{info.header}</h2>
+        <p className='mymanager-event-content mymanager-truncate'>{info.body}</p>
+        <p className='mymanager-company mymanager-truncate'>{info.footer}</p>
+      </div>
+    </div>
+  </div> */}
   return (
-    <div className='mymanager-widget-container'>
-      <div className="mymanager-widget-body">
-        <div className="mymanager-mapbox">
+    <div style={mymanagerWidgetContainer}>
+      <div style={mymanagerWidgetBody}>
+        <div style={mymanagerWidgetBody}>
           <GoogleMapReact
             bootstrapURLKeys={{ key: "AIzaSyBVrhAX-Kht3yDvmUCQHqMSeB1Qd7XVFq8" }}
             defaultCenter={pos}
             defaultZoom={11}
             // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+            style={{borderRadius: "50%"}}
           >
             <AnyReactComponent
               lat={pos.lat}
@@ -59,13 +84,10 @@ export const NotificationWrapper = ({settings, notification}) => {
             />
           </GoogleMapReact>
         </div>
-        <div className='mymanager-text-content mymanager-truncate'>
-          {/* <h2 className='mymanager-title mymanager-truncate'>{_n.type.toUpperCase()}</h2>
-          <p className='mymanager-event-content mymanager-truncate'>{_n?.msg || `VISITORS: ${_n?.message_visitors}`}</p>
-          <p className='mymanager-company mymanager-truncate'>BY www.mymanager.com</p> */}
-          <h2 className='mymanager-title mymanager-truncate'>{info.header}</h2>
-          <p className='mymanager-event-content mymanager-truncate'>{info.body}</p>
-          <p className='mymanager-company mymanager-truncate'>{info.footer}</p>
+        <div style={{...mymanagerTextContent, ...mymanagerTruncate}}>
+          <h2 style={{...mymanagerTitle, ...mymanagerTruncate}}>{info.header}</h2>
+          <p style={{...mymanagerEventContent, ...mymanagerTruncate}}>{info.body}</p>
+          <p style={{...mymanagerCompany, ...mymanagerTruncate}}>{info.footer}</p>
         </div>
       </div>
     </div>
