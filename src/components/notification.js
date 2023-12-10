d1b6c0ba4d2bc0c0952c8b3353377019434e635f
Full Stack Web & Mobile Developer <devprogrammer22@gmail.com>
2023-12-10 22:51:01 +0900

    update

--- src/components/notification.js
+++ src/components/notification.js
@@ -48,44 +48,13 @@ export const NotificationWrapper = ({settings, notification}) => {
     }
   }, [notification])
 
-  // const initializeCurrent = (lat, lng) => {
-  //   let currGeocode = new googleMapReact.maps.Geocoder()
-
-  //   if (!!lat && !!lng) {
-  //     let myLatLng = new googleMapReact.maps.LatLng(lat, lng);
-  //     return getCurrentAddress(myLatLng)
-  //   }
-  // } 
   const apiIsLoaded = (map, maps) => {
-    console.log("==map===", map)
-    console.log("==maps===", maps)
+    // console.log("==map===", map)
+    // console.log("==maps===", maps)
   }
   
   const AnyReactComponent = ({ text }) => <div>{text}</div>;
   if (!info) return <></>
-  {/*<div className='mymanager-widget-container'>
-     <div className="mymanager-widget-body">
-      <div className="mymanager-mapbox">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyBVrhAX-Kht3yDvmUCQHqMSeB1Qd7XVFq8" }}
-          defaultCenter={pos}
-          defaultZoom={11}
-          // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-        >
-          <AnyReactComponent
-            lat={pos.lat}
-            lng={pos.lng}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-      <div className='mymanager-text-content mymanager-truncate'>
-        <h2 className='mymanager-title mymanager-truncate'>{info.header}</h2>
-        <p className='mymanager-event-content mymanager-truncate'>{info.body}</p>
-        <p className='mymanager-company mymanager-truncate'>{info.footer}</p>
-      </div>
-    </div>
-  </div> */}
   return (
     <div style={mymanagerWidgetContainer}>
       <div style={mymanagerWidgetBody}>
